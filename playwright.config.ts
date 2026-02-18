5c4b7e25eea35e65986cac5bdcf1daefc9bc0b64
griezmanstha <greecemahmadridista@gmail.com>
2026-02-18 16:33:53 +0100

    Jenkinsfile added

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ const slowMo = process.env.SLOW_MO ? Number(process.env.SLOW_MO) : 0;
 const startLocalServer = baseURL === 'http://localhost:8080';
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: 'e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
